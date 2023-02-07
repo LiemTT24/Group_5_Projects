0fd96a2300200e5304588ee85d1d731c96b1e124
nguyentoann21 <nguyenvantoan28082001@gmail.com>
2023-02-07 15:57:10 +0700

    Add Models

--- Group5_API_Project/Models/Product.cs
+++ Group5_API_Project/Models/Product.cs
@@ -1,6 +1,32 @@
-﻿namespace Group5_API_Project.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Group5_API_Project.Models
 {
     public class Product
     {
+        [Key]
+        [Range(1, int.MaxValue)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProductID { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "Product name must be less than or equal 100 characters")]
+        public string? ProductName { get; set; }
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int QuantityPerUnit { get; set; }
+        [Required]
+        public decimal UnitPrice { get; set; }
+        [Required]
+        public DateTime? CreatedDate { get; set; }
+        [Required]
+        public DateTime? UpdatedDate { get; set; }
+        [ForeignKey(nameof(Suppliers))]
+        public int SupplierID { get; set; }
+        public Supplier? Suppliers { get; set; }
+        [ForeignKey(nameof(Categories))]
+        public int CategoryID { get; set; }
+        public Category? Categories { get; set; }
+
     }
 }
---
+++ Group5_API_Project/Models/RoleManager.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Group5_API_Project.Models
+{
+    public class RoleManager
+    {
+        [Key]
+        [Range(1, int.MaxValue)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int RoleManagerID { get; set; }
+        [Required]
+        [StringLength(20, ErrorMessage = "The RoleManager name must be less than or equal 20 characters")]
+        public string? RoleManagerName { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "The description must be less than or equal 100 characters")]
+        public string? Description { get; set; }
+    }
+}
